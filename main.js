a3804b3001e0db5ec70ea8892270c7dccce8eeb3
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-01-28 19:45:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,21 +6,27 @@ function start() {
     recognition.start();
 }
 
-recognition.onresult = function(e){
+recognition.onresult = function (e) {
     console.log(e);
     var content = e.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
     console.log(content);
-    speak()
+    if (content == "take my selfie") {
+        speak()
+    } else {
+        console.log("The App Works Only If You Say Take My Selfie.")
+    }
 }
 
 function speak() {
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value
-    var utterance = new SpeechSynthesisUtterance (speak_data);
+    speak_data = "Taking You Selfie In 5 Seconds";
+    var utterance = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterance);
     Webcam.attach(camera)
-    setTimeout(function() {
+    setTimeout(function () {
+        takesnapshot()
+        save()
     }, 5000);
 }
 
@@ -32,3 +38,16 @@ Webcam.set({
 });
 
 camera = document.getElementById("camera");
+
+function takesnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("img1").innerHTML = '<img id="selfieimg" src="' + data_uri + '">';
+    });
+}
+
+function save() {
+    link = document.getElementById("link");
+    img = document.getElementById("selfieimg").src;
+    link.href = img;
+    link.click();
+}
